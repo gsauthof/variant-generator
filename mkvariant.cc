d7c1415f946cd6dd4575829b854fe921897d9662
Georg Sauthoff <mail@georg.so>
2015-06-24 16:54:32 +0200

    replace $PROGRAM in GPL template

--- mkvariant.cc
+++ mkvariant.cc
@@ -34,20 +34,20 @@ static void help(const char *argv0, std::ostream &o)
 
 /* {{{ GPLv3
 
-    This file is part of the variant generator.
+    This file is part of the Variant Generator.
 
-    $PROGRAM is free software: you can redistribute it and/or modify
+    Variant Generator is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    $PROGRAM is distributed in the hope that it will be useful,
+    Variant Generator is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with $PROGRAM.  If not, see <http://www.gnu.org/licenses/>.
+    along with Variant Generator.  If not, see <http://www.gnu.org/licenses/>.
 
 }}} */
 
